f5ff7092e6335fdd0e0bc03137f0d154efd60abf
Ivan Havinson <havinsonivan@gmail.com>
2023-04-05 23:09:14 +0300

    assertion

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -6,10 +6,8 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..',
 
 from package_name.capitalize import capitalize
 
-if capitalize('hello') != 'Hello':
-    raise Exception('Функция работает неверно!')
+assert capitalize('hello') == 'Hello'
 
-if capitalize('') != '':
-    raise Exception('Функция работает неверно!')
+assert capitalize('') == ''
 
 print('Все тесты пройдены!')
