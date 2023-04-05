c6856540baee37d5d5d95e2d4462b90be7c5eba8
Ivan Havinson <havinsonivan@gmail.com>
2023-04-05 22:51:45 +0300

    imports

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,4 +1,10 @@
-from capitalize import capitalize
+import os
+import sys
+
+# Добавить путь к папке package_name в PYTHONPATH
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'package_name', '..')))
+
+from package_name.capitalize import capitalize
 
 if capitalize('hello') != 'Hello':
     raise Exception('Функция работает неверно!')
